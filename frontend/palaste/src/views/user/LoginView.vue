07903c6665eb8aedbae1edea7495c7f44ec1e05b
elihu26 <evaldelo22@jaumebalmes.net>
2025-03-13 10:33:49 +0100

    añadir logo en formularios

--- frontend/palaste/src/views/user/LoginView.vue
+++ frontend/palaste/src/views/user/LoginView.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="contenedor">
+    <div class="grid min-h-screen grid-rows-[auto_1fr] grid-cols-1">
+      <Header />
     <div class="form">
       <formLogin/>
     </div>
   </div>
+</div>
 </template>
 
 <script setup>
 import formLogin from '@/components/formLogin.vue';
+import Header from '@/components/Header.vue';
+
 </script>
 
 <style scoped>
 .contenedor{
-  background-color: rgb(15, 23, 42);
+  background-color: rgb(47, 69, 119);
   min-height: 100vh;
   justify-content: center;
   align-items: flex-start;
-  padding-top: 100px; 
 }
 .form{
   top: 10px;
 
+}
+header {
+  width: 100%;
+  padding: 5px;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- frontend/palaste/src/views/user/RegisterView.vue
+++ frontend/palaste/src/views/user/RegisterView.vue
@@ -1,25 +1,35 @@
 <template>
-    <div class="contenedor">
-      <div class="form">
-        <formRegister/>
-      </div>
+  <div class="contenedor">
+    <div class="grid min-h-screen grid-rows-[auto_1fr] grid-cols-1">
+      <Header />
+    <div class="form">
+      <formRegister/>
     </div>
-  </template>
-  
-  <script setup>
-  import formRegister from '@/components/formRegister.vue';
-  </script>
-  
-  <style scoped>
-  .contenedor{
-    background-color: rgb(15, 23, 42);
-    min-height: 100vh;
-    justify-content: center;
-    align-items: flex-start;
-    padding-top: 100px; 
-  }
-  .form{
-    top: 10px;
-  
-  }
-  </style>
+  </div>
+</div>
+</template>
+
+<script setup>
+import formRegister from '@/components/formRegister.vue';
+import Header from '@/components/Header.vue';
+
+</script>
+
+<style scoped>
+.contenedor{
+  background-color: rgb(47, 69, 119);
+  min-height: 100vh;
+  justify-content: center;
+  align-items: flex-start;
+}
+.form{
+  top: 10px;
+
+}
+header {
+  width: 100%;
+  padding: 5px;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
